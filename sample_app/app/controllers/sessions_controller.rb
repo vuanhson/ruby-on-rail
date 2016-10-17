b231fc411604c654ca03e23d3e8dfb8b9e018caf
vuson-hedspi <vuanhson@outlook.com>
2016-10-17 20:36:44 +0700

    week6

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
           log_in user
-          redirect_to user# Log the user in and redirect to the user's show page.
+          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+          redirect_back_or user# Log the user in and redirect to the user's show page.
           else
           flash.now[:danger] = 'Invalid email/password combination'# Create an error message.
           render 'new'
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url
   end
 end
--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
-  end
+      @users = User.paginate(page: params[:page])  end
 
   # GET /users/1
   # GET /users/1.json
@@ -40,25 +39,38 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+          flash[:success] = "Profile updated"
+          redirect_to @user
+          else
+          render 'edit'
       end
-    end
+  end
+  
+  def edit
+      @user = User.find(params[:id])
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
+  end
+  
+  def logged_in_user
+      unless logged_in?
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+      end
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
   end
 
   private
@@ -71,4 +83,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+   
 end
